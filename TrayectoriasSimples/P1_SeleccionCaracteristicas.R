66041306f86a2c7a7847045454924ffdccf96004
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-03-23 19:51:01 +0100

    creating feature initial representation

--- TrayectoriasSimples/P1_SeleccionCaracteristicas.R
+++ TrayectoriasSimples/P1_SeleccionCaracteristicas.R
@@ -11,18 +11,19 @@ normalize <- function(x) {
   x[is.nan(x)]<- 0
   return (x)
 } 
-#quitar colunmas que salen 0
-Aritmia=Aritmia[sapply(Aritmia, function(x) length(unique(x))>1)]
 
 #F(s)=alphaTA-(1-aplha)TR donde TR es treduccion (por ej alpha=0.8)
+#quit class column to avoid normalizing it
 nAritmia<-Aritmia[ ,-ncol(Aritmia)]
-
+#normalizing
 nAritmia<-apply(nAritmia,2,normalize)
-
-#Aritmia<-cbind(nAritmia,Aritmia$class)
-
+#adding class column to normalized data
 AritmiaNormalized<-data.frame(nAritmia,Aritmia$class)
 
+#deleting columns wich row's values are all the same
+AritmiaNormalized=AritmiaNormalized[sapply(AritmiaNormalized, function(x) length(unique(x))>1)]
+
+#creating feature representation matrix
 matrizSolucion<-matrix(0,nrow(Aritmia)/2,ncol(nAritmia))
 
 Aritmia[ ,]
